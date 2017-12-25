bde2f96f64ec12eac9ec6175eb8cb4d8b4c6f88d
evrencoskun <evrencoshkun@gmail.com>
2017-12-25 07:20:57 +0300

    NewFeature: TableAdapter has new helper methods about showing & hiding row (Working-process). This feature is might not work for now.

--- tableview/src/main/java/com/evrencoskun/tableview/handler/VisibilityHandler.java
+++ tableview/src/main/java/com/evrencoskun/tableview/handler/VisibilityHandler.java
@@ -3,6 +3,7 @@ package com.evrencoskun.tableview.handler;
 import android.view.View;
 
 import com.evrencoskun.tableview.ITableView;
+import com.evrencoskun.tableview.adapter.AbstractTableAdapter;
 import com.evrencoskun.tableview.adapter.recyclerview.CellRecyclerView;
 import com.evrencoskun.tableview.adapter.recyclerview.holder.AbstractViewHolder;
 
@@ -18,11 +19,13 @@ public class VisibilityHandler {
     private ITableView mTableView;
     private List<Integer> mHideRowList = new ArrayList<>();
     private CellRecyclerView mRowHeaderRecyclerView, mCellRecyclerView;
+    private AbstractTableAdapter mTableViewAdapter;
 
     public VisibilityHandler(ITableView pTableView) {
         this.mTableView = pTableView;
         this.mCellRecyclerView = pTableView.getCellRecyclerView();
         this.mRowHeaderRecyclerView = pTableView.getRowHeaderRecyclerView();
+        this.mTableViewAdapter = pTableView.getAdapter();
     }
 
     public void hideRow(int pYPosition) {
@@ -63,5 +66,31 @@ public class VisibilityHandler {
             rowHeaderViewHolder.itemView.setVisibility(p_bIsVisible ? View.VISIBLE : View.GONE);
             cellViewHolder.itemView.setVisibility(p_bIsVisible ? View.VISIBLE : View.GONE);
         }
+    }
+
+    class Row {
+        private int mYPosition;
+        private Object mRowHeaderModel;
+        private List<Object> mCellModelList;
+
+        public Row(int pYPosition, Object pRowHeaderModel, List<Object> pCellModelList) {
+            this.mYPosition = pYPosition;
+            this.mRowHeaderModel = pRowHeaderModel;
+            this.mCellModelList = pCellModelList;
+        }
+
+        public int getYPosition() {
+            return mYPosition;
+        }
+
+        public Object getRowHeaderModel() {
+            return mRowHeaderModel;
+        }
+
+        public List<Object> getCellModelList() {
+            return mCellModelList;
+        }
+
+
     }
 }
