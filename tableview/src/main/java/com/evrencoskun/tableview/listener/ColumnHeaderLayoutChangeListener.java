cdfe5e7d7a212d61cf8a7f217d049545982e58f6
evrencoskun <evrencoshkun@gmail.com>
2018-01-24 07:54:18 +0300

    Parent RecyclerView of Cell Row Recyclerview didn't use to remeasure itself when it is necessary. This problem has been resolved. #24 issue has been fixed.

--- tableview/src/main/java/com/evrencoskun/tableview/listener/ColumnHeaderLayoutChangeListener.java
+++ tableview/src/main/java/com/evrencoskun/tableview/listener/ColumnHeaderLayoutChangeListener.java
@@ -22,11 +22,15 @@ public class ColumnHeaderLayoutChangeListener implements View.OnLayoutChangeList
         this.mCellLayoutManager = tableView.getCellLayoutManager();
     }
 
+
     @Override
-    public void onLayoutChange(View view, int i, int i1, int i2, int i3, int i4, int i5, int i6,
-                               int i7) {
-        if (mCellRecyclerView.isShown() && mColumnHeaderRecyclerView.getWidth() >
-                mCellRecyclerView.getWidth()) {
+    public void onLayoutChange((View v, int left, int top, int right, int bottom, int oldLeft,
+                               int oldTop, int oldRight, int oldBottom) {
+        int newWidth = right - left;
+        int oldWidth = oldRight - oldLeft;
+
+        if (mCellRecyclerView.isShown() && newWidth != oldWidth && mColumnHeaderRecyclerView
+                .getWidth() > mCellRecyclerView.getWidth()) {
 
             // Remeasure all nested CellRow recyclerViews
             mCellLayoutManager.remeasureAllChild();
