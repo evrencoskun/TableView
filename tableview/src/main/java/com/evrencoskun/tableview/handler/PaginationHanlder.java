fd43a70120cf0b239c76ee6f5b4797b98b99e375
Jeremy Patrick Pacabis <jeremy@ingenuity.ph>
2018-02-04 11:30:51 +0800

    corrected comment on update method

---
+++ tableview/src/main/java/com/evrencoskun/tableview/handler/PaginationHanlder.java
@@ -0,0 +1,11 @@
+package com.evrencoskun.tableview.handler;
+
+/**
+ * Created by jeremypacabis on February 04, 2018.
+ *
+ * @author Jeremy Patrick Pacabis <jeremy@ingenuity.ph>
+ *         com.evrencoskun.tableview.handler <TableView>
+ */
+
+public class PaginationHanlder {
+}
---
+++ tableview/src/main/java/com/evrencoskun/tableview/pagination/Pagination.java
@@ -0,0 +1,11 @@
+package com.evrencoskun.tableview.pagination;
+
+/**
+ * Created by jeremypacabis on February 04, 2018.
+ *
+ * @author Jeremy Patrick Pacabis <jeremy@ingenuity.ph>
+ *         com.evrencoskun.tableview.pagination <TableView>
+ */
+
+public class Pagination {
+}
