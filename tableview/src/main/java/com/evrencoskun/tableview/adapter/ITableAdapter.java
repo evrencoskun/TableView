a9d5be2fd904b2c1dc820874af526aab241df75c
Andhie <andhie@agmostudio.com>
2018-09-04 20:39:39 +0800

    Dont require extra casting

--- tableview/src/main/java/com/evrencoskun/tableview/adapter/ITableAdapter.java
+++ tableview/src/main/java/com/evrencoskun/tableview/adapter/ITableAdapter.java
@@ -27,7 +27,7 @@ import com.evrencoskun.tableview.adapter.recyclerview.holder.AbstractViewHolder;
  * Created by evrencoskun on 10/06/2017.
  */
 
-public interface ITableAdapter {
+public interface ITableAdapter<CH, RH, C> {
 
     int getColumnHeaderItemViewType(int position);
 
@@ -39,17 +39,17 @@ public interface ITableAdapter {
 
     AbstractViewHolder onCreateCellViewHolder(ViewGroup parent, int viewType);
 
-    void onBindCellViewHolder(AbstractViewHolder holder, Object cellItemModel, int
+    void onBindCellViewHolder(AbstractViewHolder holder, C cellItemModel, int
             columnPosition, int rowPosition);
 
     AbstractViewHolder onCreateColumnHeaderViewHolder(ViewGroup parent, int viewType);
 
-    void onBindColumnHeaderViewHolder(AbstractViewHolder holder, Object columnHeaderItemModel,
+    void onBindColumnHeaderViewHolder(AbstractViewHolder holder, CH columnHeaderItemModel,
                                       int columnPosition);
 
     AbstractViewHolder onCreateRowHeaderViewHolder(ViewGroup parent, int viewType);
 
-    void onBindRowHeaderViewHolder(AbstractViewHolder holder, Object rowHeaderItemModel, int
+    void onBindRowHeaderViewHolder(AbstractViewHolder holder, RH rowHeaderItemModel, int
             rowPosition);
 
     View onCreateCornerView();
