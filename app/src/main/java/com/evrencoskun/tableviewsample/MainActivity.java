4209dd02e193db6c067d7bf8dd239ba7d15d0ff6
Gaëtan Muller <m.gaetan89@gmail.com>
2020-03-31 08:19:05 +0200

    Simplify MainActivity

--- app/src/main/java/com/evrencoskun/tableviewsample/MainActivity.java
+++ app/src/main/java/com/evrencoskun/tableviewsample/MainActivity.java
@@ -17,16 +17,10 @@
 
 package com.evrencoskun.tableviewsample;
 
-import android.os.Bundle;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public MainActivity() {
+        super(R.layout.activity_main);
     }
 }
