027800cdcde153378664f63970db3d47bf950236
Andrew Beck <>
2020-06-29 23:40:43 +0100

    Add CornerView Tests Enable Jacoco Code Coverage on tableview Instrumented Tests by running "createDebugCoverageReport" gradle tasks

--- tableview/src/androidTest/java/com/evrencoskun/tableview/test/data/SimpleData.java
+++ tableview/src/androidTest/java/com/evrencoskun/tableview/test/data/SimpleData.java
@@ -1,16 +1,25 @@
 /*
- * Copyright (c) 2020. Andrew Beck
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * MIT License
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * Copyright (c) 2020 Andrew Beck
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 package com.evrencoskun.tableview.test.data;
@@ -27,21 +36,29 @@ public class SimpleData {
     private List<ColumnHeader> columnHeaders;
     private List<RowHeader> rowHeaders;
 
-    public SimpleData(int size) {
+    public SimpleData(int size){
+        init(size, size);
+    }
+
+    public SimpleData(int columnSize, int rowSize) {
+        init(columnSize, rowSize);
+    }
+
+    private void init(int columnSize, int rowSize) {
         rowHeaders = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < rowSize; i++) {
             rowHeaders.add(new RowHeader(String.valueOf(i), "r:" + i));
         }
 
         columnHeaders = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < columnSize; i++) {
             columnHeaders.add(new ColumnHeader(String.valueOf(i), "c:" + i));
         }
 
         cells = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < rowSize; i++) {
             ArrayList<Cell> cellList = new ArrayList<>();
-            for (int j = 0; j < size; j++) {
+            for (int j = 0; j < columnSize; j++) {
                 String id = j + ":" + i;
                 cellList.add(new Cell(id, "r:" + i + "c:" + j));
             }
@@ -53,11 +70,23 @@ public class SimpleData {
         return cells;
     }
 
+    public void setCells(List<List<Cell>> cells) {
+        this.cells = cells;
+    }
+
     public List<ColumnHeader> getColumnHeaders() {
         return columnHeaders;
     }
 
+    public void setColumnHeaders(List<ColumnHeader> columnHeaders) {
+        this.columnHeaders = columnHeaders;
+    }
+
     public List<RowHeader> getRowHeaders() {
         return rowHeaders;
+    }
+
+    public void setRowHeaders(List<RowHeader> rowHeaders) {
+        this.rowHeaders = rowHeaders;
     }
 }
